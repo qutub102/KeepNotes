5bd46a46ac947065e0c13c6ceecfecc631f51ce8
Qutubuddin <qutubuddiniam@gmail.com>
2020-06-27 17:22:36 +0530

    Updates

---
+++ precache-manifest.6ad4006ca9853eeb6b9a59f87b968855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83a915837a1e2c68495dafa9e92b418",
+    "url": "/KeepNotes/index.html"
+  },
+  {
+    "revision": "620884a7f5677f9cadbb",
+    "url": "/KeepNotes/static/css/main.453dbb3f.chunk.css"
+  },
+  {
+    "revision": "e9d138fd6fa27650230f",
+    "url": "/KeepNotes/static/js/2.8d5570c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/KeepNotes/static/js/2.8d5570c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620884a7f5677f9cadbb",
+    "url": "/KeepNotes/static/js/main.50e2e7b3.chunk.js"
+  },
+  {
+    "revision": "45e41294a9217a271c32",
+    "url": "/KeepNotes/static/js/runtime-main.c3e64943.js"
+  }
+]);
