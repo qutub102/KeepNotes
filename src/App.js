2b8104249ce7bc0346df60d149858491081e5240
Qutubuddin <qutubuddiniam@gmail.com>
2020-06-29 21:49:19 +0530

    Changes in Search

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ const App = () => {
     Array.from(noteCard).forEach(function(element){
       let noteContent = element.getElementsByTagName('p')[0].innerText;
       let noteTitle = element.getElementsByTagName('h3')[0].innerText;
-      if(noteContent.includes(inputsrch) || noteTitle.includes(inputsrch)){
+      if(noteContent.toLowerCase().includes(inputsrch) || noteTitle.toLowerCase().includes(inputsrch)){
         element.style.display = "block";
       }
       else{
