acec3f2299fdc6440d6aa8d69b683fe1117ec9b4
Qutubuddin <qutubuddiniam@gmail.com>
2020-07-05 20:44:39 +0530

    revert again

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import './index.css'
 const Header = (props) => {
   const srchEvent = (event) => props.srchEven(event);
   return (
